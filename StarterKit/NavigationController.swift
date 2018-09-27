172846bdf0e544f2ccb3c3d65c29972f1b67f5a6
Alexandrug Ghiura <ghalex@gmail.com>
2018-09-27 15:57:32 +0300

    Create Login & Signup view controllers

---
+++ StarterKit/NavigationController.swift
@@ -0,0 +1,21 @@
+//
+//  NavigationController.swift
+//  StarterKit
+//
+//  Created by Alexandru Ghiura on 27/09/2018.
+//  Copyright © 2018 Alexandru Ghiura. All rights reserved.
+//
+
+import UIKit
+
+class NavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        //navigationBar.isTranslucent = true
+        //navigationBar.setBackgroundImage(UIImage(), for: .default)
+        //navigationBar.shadowImage = UIImage()
+    }
+}
--- StarterKit/Views/WelcomeViewController.swift
+++ StarterKit/Views/WelcomeViewController.swift
@@ -10,9 +10,31 @@ import UIKit
 
 class WelcomeViewController: UIViewController {
     
+    @IBOutlet weak var btnFacebook: Button!
+    @IBOutlet weak var btnLogin: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // self.view.addVerticalGradientLayer(topColor: UIColor(named: "Grey")!, bottomColor: UIColor(named: "White")!)
+        btnFacebook.imageView?.contentMode = .scaleAspectFit
+        btnFacebook.imageView?.tintColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        btnFacebook.imageEdgeInsets = UIEdgeInsets.init(top: 10, left: 10, bottom: 10, right: 10)
+        
+        btnLogin.semanticContentAttribute = UIApplication.shared
+            .userInterfaceLayoutDirection == .rightToLeft ? .forceLeftToRight : .forceRightToLeft
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+         navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
     }
 
 }
